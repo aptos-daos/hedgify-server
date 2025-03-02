1fc13fcabfe1216b698aa44656ce2379e627481c
Arup Basak <email.arupbasak@gmail.com>
2025-03-02 12:08:10 +0530

    turned down worker

--- src/libs/workers.ts
+++ src/libs/workers.ts
@@ -2,7 +2,7 @@ import { LikeQueueWorker } from "../workers/like-queue.worker";
 
 const likeQueueWorker = new LikeQueueWorker();
 
-likeQueueWorker
-  .start()
-  .then(() => console.log("⚡️[WORKER]: Like Queue Worker Started"))
-  .catch(console.error);
+// likeQueueWorker
+//   .start()
+//   .then(() => console.log("⚡️[WORKER]: Like Queue Worker Started"))
+//   .catch(console.error);
